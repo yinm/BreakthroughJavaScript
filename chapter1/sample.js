527eb521ca49797907d5f259c997e6d5775714cc
yinm <yinm.cminor9@gmail.com>
2017-05-30 07:18:41 +0900

    クロージャを使わない問題点の確認
    同じ処理の記述が増えてしまう
    変数にアクセスできるため、意図しない変更が加えられる可能性がある

--- chapter1/sample.js
+++ chapter1/sample.js
@@ -1,9 +1,21 @@
-let count = 0;
-
-function counter() {
-  count++;
-  console.log(count);
+let count1 = 0;
+function counter1() {
+  count1++;
+  console.log(count1);
 }
 
-counter();
-counter();
+let count2 = 0;
+function counter2() {
+  count2++;
+  console.log(count2);
+}
+
+counter1();
+counter1();
+counter2();
+counter2();
+
+count1 = 100;
+
+counter1();
+counter1();

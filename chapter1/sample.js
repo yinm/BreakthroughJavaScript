4cf525ca95058c0250bc875033d6c873722424b3
yinm <yinm.cminor9@gmail.com>
2017-06-01 08:30:24 +0900

    メソッド呼び出しのthisの確認

--- chapter1/sample.js
+++ chapter1/sample.js
@@ -1,59 +1,10 @@
-function Observer() {
-  this.listeners = {};
+function Human(name) {
+  this.name = name;
 }
 
-Observer.prototype.on = function(event, func) {
-  if (!this.listeners[event]) {
-    this.listeners[event] = [];
-  }
-  this.listeners[event].push(func);
+Human.prototype.greet = function() {
+  console.log('Hello ' + this.name);
 };
 
-Observer.prototype.off = function(event, func) {
-  let ref = this.listeners[event];
-  let length = ref.length;
-  for (let i = 0; i < length; i++) {
-
-    let listener = ref[i];
-    if (listener === func) {
-      ref.splice(i, 1);
-    }
-  }
-};
-
-Observer.prototype.trigger = function(event) {
-  let ref = this.listeners[event];
-  let length = ref.length;
-  for (let i = 0; i < length; i++) {
-
-    let listener = ref[i];
-    if (typeof listener === 'function') {
-      listener();
-    }
-  }
-};
-
-let observer = new Observer();
-
-// morning イベント
-let greet = function() {
-  console.log('Good morning');
-};
-
-let wakeup = function() {
-  console.log('起きる');
-};
-
-observer.on('morning', greet);
-observer.on('morning', wakeup);
-observer.trigger('morning');
-observer.off('morning', wakeup);
-observer.trigger('morning');
-
-// evening イベント
-let sayEvening = function() {
-  console.log('Good evening');
-};
-
-observer.on('evening', sayEvening());
-observer.trigger('evening');
+let mike = new Human('Mike');
+mike.greet();

f9123fe7c5f581d882547a439b780e8cde7dca39
yinm <yinm.cminor9@gmail.com>
2017-06-04 12:42:06 +0900

    3週目

--- chapter1/sample.js
+++ chapter1/sample.js
@@ -22,21 +22,27 @@ AppModel.prototype.trigger = function(event) {
 };
 
 AppModel.prototype.set = function(val) {
-  if (this.val === val) return;
+  if (this.val === val) {
+    return;
+  }
+
   this.val = val;
   this.validate();
 };
 
-AppModel.prototype.initialize = function(el) {
-  this.$el = $(el);
+AppModel.prototype.validate = function() {
+  let val;
+  this.errors = [];
 
-  let obj = this.$el.data();
+  for (let key in this.attrs) {
+    val = this.attrs[key];
 
-  if (this.$el.prop('required')) {
-    obj['required'] = '';
+    if (!this[key](val)) {
+      this.errors.push(key);
+    }
   }
 
-  this.model = new AppModel(obj);
+  this.trigger(!this.errors.length ? 'valid' : 'invalid');
 };
 
 AppModel.prototype.required = function() {
@@ -51,30 +57,19 @@ AppModel.prototype.minlength = function(num) {
   return num <= this.val.length;
 };
 
-AppModel.prototype.validate = function() {
-  let val;
-  this.errors = [];
 
-  for (let key in this.attrs) {
-    val = this.attrs[key];
-    if (!this[key](val)) this.errors.push(key);
-  }
-
-  this.trigger(!this.errors.length ? 'valid' : 'invalid');
-};
-
-function AppView(el) {
-  this.initialize(el);
+function AppView(element) {
+  this.initialize(element);
   this.handleEvents();
 }
 
-AppView.prototype.initialize = function(el) {
-  this.$el = $(el);
-  this.$list = this.$el.next().children();
+AppView.prototype.initialize = function(element) {
+  this.$element = $(element);
+  this.$list = this.$element.next().children();
 
-  let obj = this.$el.data();
+  let obj = this.$element.data();
 
-  if (this.$el.prop('required')) {
+  if (this.$element.prop('required')) {
     obj['required'] = '';
   }
 
@@ -84,7 +79,7 @@ AppView.prototype.initialize = function(el) {
 AppView.prototype.handleEvents = function() {
   let self = this;
 
-  this.$el.on('keyup', function(e) {
+  this.$element.on('keyup', function(e) {
     self.onKeyup(e);
   });
 
@@ -103,13 +98,13 @@ AppView.prototype.onKeyup = function(e) {
 };
 
 AppView.prototype.onValid = function() {
-  this.$el.removeClass('error');
+  this.$element.removeClass('error');
   this.$list.hide();
 };
 
 AppView.prototype.onInvalid = function() {
   let self = this;
-  this.$el.addClass('error');
+  this.$element.addClass('error');
   this.$list.hide();
 
   $.each(this.model.errors, function(index, val) {
@@ -120,4 +115,3 @@ AppView.prototype.onInvalid = function() {
 $('input').each(function() {
   new AppView(this);
 });
-

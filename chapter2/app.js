bf176ab9827b153b05b0d663cfe744a457b44eb8
yinm <yinm@pepabo.com>
2017-06-06 09:49:44 +0900

    resizeが使える

--- chapter2/app.js
+++ chapter2/app.js
@@ -42,6 +42,10 @@ Modal.prototype.handleEvents = function() {
     self.prev(e);
     return false;
   });
+
+  this.$window.on('resize', function() {
+    self.resize();
+  });
 };
 
 Modal.prototype.show = function(e) {
@@ -83,6 +87,16 @@ Modal.prototype.next = function() {
 
 Modal.prototype.prev = function() {
   this.slide(this.countChange(-1));
+};
+
+Modal.prototype.resize = function() {
+  let w = this.$window.width();
+
+  if (w < 640) {
+    this.$container.css({'width': '320', 'height': '213'});
+  } else {
+    this.$container.css({'width': '750', 'height': '500'});
+  }
 };
 
 let modal = new Modal($('#modal-thumb a'));

9442afb44b54276b202cce6327c97107de4c10ad
yinm <yinm.cminor9@gmail.com>
2017-06-08 08:22:06 +0900

    resolve済みのpromiseを渡す

--- chapter4/app.js
+++ chapter4/app.js
@@ -1,14 +1,16 @@
-let deferred = new $.Deferred();
+new $.Deferred().resolve().promise().then(function() {
+  let deferred = new $.Deferred();
 
-$('body').animate({
-  marginTop: 100
-}, {
-  duration: 1000,
-  complete: function() {
-    deferred.resolve();
-  }
-});
+  $('body').animate({
+    marginTop: 100
+  }, {
+    duration: 1000,
+    complete: function() {
+      deferred.resolve();
+    }
+  });
 
-deferred.promise().then(function() {
+  return deferred.promise()
+}).then(function() {
   console.log('done');
 });

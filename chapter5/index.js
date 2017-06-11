9d9a653a8be3d7991705704b5f48e3287a12bde7
yinm <yinm.cminor9@gmail.com>
2017-06-12 08:00:49 +0900

    ハッシュごとのページを表示するようにする

--- chapter5/index.js
+++ chapter5/index.js
@@ -3,11 +3,18 @@
 
   function init() {
     $pages = $('[data-role="page"]').detach();
-    $(window).on('hashchange', urlChangeHandler);
+    $(window)
+      .on('hashchange', urlChangeHandler)
+      .trigger('hashchange');
   }
 
   function urlChangeHandler() {
-    alert(location.hash);
+    let pageid = parseUrl(location.hash);
+    $pages.filter('.page' + pageid).appendTo('article');
+  }
+
+  function parseUrl(url) {
+    return url.slice(1);
   }
 
   init();

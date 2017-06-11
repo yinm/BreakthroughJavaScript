72afeae57d3b649ae27d5aa4466f076142f80e3f
yinm <yinm.cminor9@gmail.com>
2017-06-12 07:55:45 +0900

    コンテンツを非表示にする

--- chapter5/index.js
+++ chapter5/index.js
@@ -1,5 +1,12 @@
+let $page;
+
+function init() {
+  $pages = $('[data-role="page"]').detach();
+  $(window).on('hashchange', urlChangeHandler);
+}
+
 function urlChangeHandler() {
   alert(location.hash);
 }
 
-$(window).on('hashchange', urlChangeHandler);
+init();

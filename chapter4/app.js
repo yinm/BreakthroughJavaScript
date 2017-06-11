4ea92cf9d0a15a7d0d11d6e26c4902f75fafc032
yinm <yinm.cminor9@gmail.com>
2017-06-11 11:10:37 +0900

    解決済みのpromiseを渡すパターン

--- chapter4/app.js
+++ chapter4/app.js
@@ -1,14 +1,14 @@
-let deferred = new $.Deferred();
-
-$('body').animate({
-  marginTop: 100
-}, {
-  duration: 1000,
-  complete: function() {
-    deferred.resolve();
-  }
-});
-
-deferred.promise().then(function() {
+new $.Deferred().resolve().promise().then(function() {
+  let deferred = new $.Deferred();
+  $('body').animate({
+    marginTop: 100
+  }, {
+    duration: 1000,
+    complete: function() {
+      deferred.resolve();
+    }
+  });
+  return deferred.promise();
+}).then(function() {
   console.log('done');
 });

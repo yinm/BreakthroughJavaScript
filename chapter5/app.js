7b062987649e11c475d00fbd4d4b7fc656f01add
yinm <yinm.cminor9@gmail.com>
2017-06-11 12:20:07 +0900

    即時関数で、変数のスコープを限定する

--- chapter5/app.js
+++ chapter5/app.js
@@ -1,12 +1,14 @@
-function urlChangeHandler() {
-  alert(location.hash);
-}
+;(function() {
+  let $pages;
 
-let $pages;
+  function urlChangeHandler() {
+    alert(location.hash);
+  }
 
-function init() {
-  $pages = $('[data-role="page"]').detach();
-  $(window).on('hashchange', urlChangeHandler);
-}
+  function init() {
+    $pages = $('[data-role="page"]').detach();
+    $(window).on('hashchange', urlChangeHandler);
+  }
 
-init();
+  init();
+})();
